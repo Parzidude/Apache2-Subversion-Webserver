5823415caf5b5df4475e87b7fcb577b3db1de679
nikolay-marinov <n@repulse.com>
2022-01-10 19:20:12 +0200

    Fixed line endings on scrips

--- SubversionApache2/Startup.sh
+++ SubversionApache2/Startup.sh
@@ -1,8 +1,7 @@
-mkdir -p /var/lib/svn/ 
-svnadmin create /var/lib/svn/trunk 
-chown -R www-data:www-data /var/lib/svn 
-chmod -R 775 /var/lib/svn 
+#!/bin/bash
+
+svnadmin create /var/lib/svn/trunk
+chown -R www-data:www-data /var/lib/svn
+chmod -R 775 /var/lib/svn
 service apache2 start
-while true; do
-  sleep 1000
-done
+exec tail -f /dev/null
